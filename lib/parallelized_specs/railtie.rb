885165407491201f04e720e56ed2dd955f192be7
Brian Palmer <brianp@instructure.com>
2013-03-21 16:57:23 -0600

    fix incorrect load path for rails3 railtie
    this was preventing test mode bootup under rails 3

--- lib/parallelized_specs/railtie.rb
+++ lib/parallelized_specs/railtie.rb
@@ -3,7 +3,7 @@ if defined?(Rails::Railtie)
   class ParallelizedSpecs
     class Railtie < ::Rails::Railtie
       rake_tasks do
-        load File.expand_path("../tasks/parallelized_specs.rake", __FILE__)
+        load File.expand_path("../../tasks/parallelized_specs.rake", __FILE__)
       end
     end
   end
